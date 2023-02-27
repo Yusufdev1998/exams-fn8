ccb73cba50c39c0b247acb3028f428e8d7675432
Yusufdev1998 <yusufdev1998@gmail.com>
2023-02-27 23:36:32 +0500

    exam fn8

---
+++ 3-month-exam.js
@@ -0,0 +1,67 @@
+/*
+   !!! Masalalarni ishlayotganda js faylni htmlga ulash mumkin emas.
+   Ya'ni kodni tekshirib ko'rish man etiladi.
+   Internetdagi ma'lumotlardan xohlagancha foydalanish mumkin.
+*/
+
+/*
+   1-masala.(10 ball)
+   Dollarni so'mga o'girib beradigan dastur tuzing.
+   Bunda prompt orqali foydalanuvchidan "Sizda necha dollar bor ?" deb so'raladi.
+   Va dastur dollarni so'mga o'girib, natijani consolega chiqarib bersin.
+   Bunda kursni 11400so'm deb oling.
+
+   Masalan: promptga 2 dollar kiritilsa, natija 22800so'm consolega chiqadi.
+*/
+
+/* 
+  1-masalaning kodini ushbu kommentning o'rnida yozing.
+  1-masalani ishlab bo'lgandan keyin uni kommentga olib qoying.
+*/
+
+/*
+    2-masala.(20 ball)
+    Alisher chet elga dam olishga bormoqchi. 
+    Safar uchun ketadigan xarajatni internetdagi saytlardan qidirib chiqib, 
+    quyidagi ro’yxatda berilgan ma’lumotlarni topdi. 
+    Ularning ba’zilari AQSh dollarida ko’rsatilgan bo’lsa, 
+    ba’zilari yevroda berilgan.
+
+    - Borish-kelish samolyot bileti - $500
+    - Mehmonxona to’lovi (to’liq safar davomiyligi uchun) - $250
+    - Muzey va ko’ngilochar joylar uchun - 120 yevro
+
+    $1 = 11000 so’m
+    1 yevro = 12354 so’m
+
+    Tuzilishi kerak bo’lgan dastur quyidagi ko’rinishda ishlaydi:
+    1. Alisher o’zida necha pul bor ekanligini so’mda prompt orqali kiritadi.
+    2. Alisherda yetarlicha pul bo’lsa, 
+    console.log da “Oq yo’l, Alisher!” xabari chiqadi.
+    3. Alisherda yetarlicha pul bo’lmasa, 
+    console.log da “Alisher, ozgina sabr qilish kerak bo’lar ekan.” xabari chiqadi.
+
+*/
+
+/* 
+  2-masalaning kodini bu yerda yozing.
+  2-masalani ishlab bo'lgandan keyin uni kommentga olib qoying.
+*/
+
+/*
+    3-masala.(30 ball)
+    Prompt orqali sekund kiritiladi.
+    Masalan 120 sekund.
+    Dastur ushbu sekundni minut va sekundlarda ifodalasin.
+    Ya'ni natija 00:00 formatda bo'lsin. Natijani consolega chiqaring.
+
+    1-namuna: 130 sekund kiritilsa, natija 02:10 
+    2-namuna: 65 sekund kiritilsa, natija 01:05
+    3-namuna: 40 sekund kiritilsa, natija 00:40
+    4-namuna: 2000 sekund kiritilsa, natija 33:20
+*/
+
+/* 
+  3-masalaning kodini bu yerda yozing.
+  3-masalani ishlab bo'lgandan keyin uni kommentga olib qoying.
+*/
