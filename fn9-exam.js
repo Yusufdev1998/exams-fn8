adce5ffcfa6446ae8fae1a4576adde80a19976cc
Yusufdev1998 <yusufdev1998@gmail.com>
2023-03-21 09:47:10 +0500

    fn9 3 oy imtihon

---
+++ fn9-exam.js
@@ -0,0 +1,51 @@
+/*
+   !!! Masalalarni ishlayotganda js faylni htmlga ulash mumkin emas.
+   Ya'ni kodni tekshirib ko'rish man etiladi.
+   Internetdagi ma'lumotlardan xohlagancha foydalanish mumkin.
+*/
+
+/*
+   1-masala.(10 ball)
+   Killometrdan metrga o'tkizadigan funksiya yarating.
+
+   Ya'ni funksiyaga kilometr argument sifatida berilsa,
+   ushbu funksiya metr qaytarsin. Masalan: 
+   1. 2km kiritilsa, va funksiya chaqirilsa, 2000m qaytsin
+
+   
+   function kmToM(km){
+     let m;
+     ...code
+     return m
+   }
+
+*/
+
+/* 
+  1-masalaning kodini ushbu kommentning o'rnida yozing.
+  1-masalani ishlab bo'lgandan keyin uni kommentga olib qoying.
+*/
+
+/*
+    2-masala.(20 ball)
+    Promptdan gap kiritilsa, agar ushbu gap kichik harf bilan
+    boshlangan bo'lsa, uning birinchi harfini bosh harfga o'giring.
+    Agar gap nuqta bilan tugamagan bo'lsa gapning oxiriga nuqta qo'yin.
+    Oxirida gapni consolega chiqaring.
+*/
+
+/* 
+  2-masalaning kodini bu yerda yozing.
+  2-masalani ishlab bo'lgandan keyin uni kommentga olib qoying.
+*/
+
+/*
+    3-masala.(30 ball)(real loyihadan)
+    0,01 to 49,99 gacha bo'lgan sonlar ichidan
+    taxminiy son chiqaradigan algoritmni tuzing
+*/
+
+/* 
+  3-masalaning kodini bu yerda yozing.
+  3-masalani ishlab bo'lgandan keyin uni kommentga olib qoying.
+*/
